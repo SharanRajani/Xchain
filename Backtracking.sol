fbcf526919018ce159cd1d6e4b50b772403c5c16
thegodfather101 <thesharanrajani@gmail.com>
2017-12-21 19:16:01 +0530

    Backtracking logic done but not tested.

--- Backtracking.sol
+++ Backtracking.sol
@@ -4,4 +4,18 @@ import "./Basic.sol";
 
 contract Backtracking is Basic {
 
+  function track(string itemId) returns (bytes32[128] ownerAddresses,uint count) {
+    uint count=0;
+    bytes32[128] ownerAddresses;
+    bytes32 trackingId = items[itemId];
+    string root = getRoot();
+    while( trackingId != root )
+    {
+      ProofEntry p1= proofs[trackingId];
+      trackingId = p1.previousTrackingId;
+      ownerAddresses[count++]=p1.owner;
+    }
+    count--;
+  }
+
 }
