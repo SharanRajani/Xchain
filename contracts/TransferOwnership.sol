d923a4ba65cce640e9c7cc4db3f9bddb70f0c330
Soham Ray <sohamray19@gmail.com>
2017-12-22 19:43:39 +0530

    Transfer done

---
+++ contracts/TransferOwnership.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.4;
+
+import "./Basic.sol";
+import "./encrypted.sol";
+
+
+contract TransferOwnership is Basic, encrypted{
+
+  function transferProof(bytes32 owner, bytes32 previousTrackingId, bytes32 newOwner)public returns(bool success) {
+
+    bytes32 trackingId=keccak256(bytes32toString(previousTrackingId));
+    bytes32 newTrackingId=keccak256(bytes32toString(trackingId));
+
+    if (keccak256(trackingId) != keccak256("root")) {
+      if (hasProof(trackingId)) {
+        ProofEntry memory pe = getProofInternal(trackingId);
+
+      }
+
+      if (hasProof(newTrackingId)) {
+        // already exists- return
+        return false;
+      }
+
+    proofs[newTrackingId].owner = newOwner;
+    proofs[newTrackingId].privateKey=register[newOwner].privateKey;
+    proofs[newTrackingId].previousTrackingId=trackingId;
+    transferCompleted(owner, newOwner, newTrackingId, proofs[newTrackingId].previousTrackingId);
+    return true;
+
+   }
+  }
+ }
