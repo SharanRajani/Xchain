44bd99e0e73a0da6bbbdc304517edb97f41532b2
thegodfather101 <thesharanrajani@gmail.com>
2017-12-23 13:27:07 +0530

    Solving Encrypted.sol import issue in TransferOwnership.sol

--- contracts/TransferOwnership.sol
+++ contracts/TransferOwnership.sol
@@ -4,7 +4,7 @@ import "./Basic.sol";
 import "./Encrypted.sol";
 
 
-contract TransferOwnership is Basic, encrypted{
+contract TransferOwnership is Basic, encrypt{
 
   function transferProof(bytes32 owner, bytes32 previousTrackingId, bytes32 newOwner, string productId)public returns(bool success) {
 
@@ -15,7 +15,7 @@ contract TransferOwnership is Basic, encrypted{
     {
       bytes32 trackingId=keccak256(productId);
     }
-    bytes32 newTrackingId=keccak256(bytes32toString(trackingId));  
+    bytes32 newTrackingId=keccak256(bytes32toString(trackingId));
 
    if (hasProof(newTrackingId)) {
     // already exists- return
